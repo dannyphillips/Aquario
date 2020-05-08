ddcbc0d65705333f492936f69a8f40941a85b1a5
Danny Phillips <danny.phillips8@gmail.com>
2020-05-07 22:06:39 -0700

    got gatsby running

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCkyh47ruV3Bf5FfGo1_akiRzM4KT8a7sc",
+  authDomain: "aquario-d29bc.firebaseapp.com",
+  databaseURL: "https://aquario-d29bc.firebaseio.com",
+  projectId: "aquario-d29bc",
+  storageBucket: "aquario-d29bc.appspot.com",
+  messagingSenderId: "13106667507",
+  appId: "1:13106667507:web:7bdd1d458df1c6c2f37c86",
+  measurementId: "G-7XDWNBJ9JS",
+};
